6b616541f1e5ec384b31533264a967ad2bffae57
oganov2001 <georgeoganov@hotmail.com>
2022-01-26 18:59:27 +0200

    Added PermMissingElement

---
+++ PermMissingElement/index.js
@@ -0,0 +1,15 @@
+function solution(A) {
+  let actualSum = 0;
+  A.forEach(number => actualSum += number);
+
+  let maxNumber = A.length + 1;
+  let expectedSum = (maxNumber * (maxNumber + 1) / 2);
+
+  return expectedSum - actualSum;
+}
+
+console.log(solution([2, 3, 1, 5]));
+
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+console.log(solution([]));
