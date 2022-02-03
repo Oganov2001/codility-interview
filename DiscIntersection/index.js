1821d8f1a9e0f6462688db83add56a389e1596f0
oganov2001 <georgeoganov@hotmail.com>
2022-02-03 16:49:07 +0200

    Added DiscIntersection

---
+++ DiscIntersection/index.js
@@ -0,0 +1,32 @@
+function solution(A) {
+  let len = A.length;
+  let discHistory = new Array(len * 2);
+  let j = 0;
+  for (let i = 0; i < len; i++) {
+    discHistory[j++] = new DiscLog(i - A[i], 1);
+    discHistory[j++] = new DiscLog(i + A[i], -1);
+  }
+  discHistory.sort(compare);
+  let intersections = 0;
+  let activeIntersections = 0;
+  for (const log of discHistory) {
+    activeIntersections += log.startEnd;
+    if (log.startEnd > 0) intersections += activeIntersections - 1;
+    if (intersections > 10000000) return -1;
+  }
+  return intersections;
+}
+
+class DiscLog {
+  constructor(x, startEnd) {
+    this.x = x;
+    this.startEnd = startEnd;
+  }
+}
+
+// a < b -> -ve, a === b -> 0, a > b -> +ve
+function compare(a, b) {
+  return a.x !== b.x ? a.x - b.x : b.startEnd - a.startEnd;
+}
+
+console.log(solution([1, 5, 2, 1, 4, 0]));
