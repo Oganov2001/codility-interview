f047de51705c2c05ad13d270539cf4971e100631
oganov2001 <georgeoganov@hotmail.com>
2022-02-04 17:20:38 +0200

    Added Passing Cars

---
+++ PassingCars/index.js
@@ -0,0 +1,12 @@
+function solution(A) {
+  let suffixSum = new Array(A.length + 1).fill(0);
+  let count = 0;
+  for (let i = A.length - 1; i >= 0; i--) {
+    suffixSum[i] = A[i] + suffixSum[i + 1];
+    if (A[i] === 0) count += suffixSum[i];
+    if (count > 10000000) return -1;
+  }
+  return count;
+}
+
+console.log(solution([0, 1, 0, 1, 1]));
